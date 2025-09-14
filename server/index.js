e76beb26c12667e152748a33f0387b0786d45577
YunVisal <visalyun1407@gmail.com>
2025-09-14 11:20:16 +0700

    add dotenv

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const app = express();
 
+require('dotenv').config();
+
 app.get('/', (req, res) => {
     res.send({"hi": "there"});
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
